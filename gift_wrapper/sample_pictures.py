fd25aaec2108adee1ec527c8286c510c671f50a6
s-marika <s177107x@st.go.tuat.ac.jp>
2020-10-23 17:39:42 +0900

    Contenius image program

--- gift_wrapper/sample_pictures.py
+++ gift_wrapper/sample_pictures.py
@@ -6,8 +6,10 @@ import sys
 import numpy as np
 from PyQt5.QtWidgets import (QWidget, QHBoxLayout,
                              QLabel, QApplication)
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtGui import QPixmap, QImage, QTransform
 import cv2
+from PIL import Image
+from PIL.ImageQt import ImageQt
 
 
 class Example(QWidget):
@@ -20,28 +22,40 @@ class Example(QWidget):
 
     def initUI(self):
 
-        self.hbox = QHBoxLayout(self)
+        self.imageArea = QHBoxLayout(self)
         # QPixmapオブジェクト作成
 
         # ラベルを作ってその中に画像を置く
         self.gazou()
-        self.setLayout(self.hbox)
+        self.setLayout(self.imageArea)
         self.move(300, 200)
         self.setWindowTitle('Imoyokan')
         self.show()
 
     def gazou(self):
-        cvImg = cv2.imread("./pictures/sample.png")
-        print(cvImg)
-        height, width, channel = cvImg.shape
-        bytesPerLine = 3 * width
-        qImg = QImage(cvImg.data, width, height,
-                      bytesPerLine, QImage.Format_RGB888).rgbSwapped()
-        pixmap01 = QPixmap.fromImage(qImg)
         lbl = QLabel(self)
-
-        lbl.setPixmap(pixmap01)
-        self.hbox.addWidget(lbl)
+        #cvImg = cv2.imread("./pictures/sample.png")
+        #print(cvImg)
+        #height, width, channel = cvImg.shape
+        #bytesPerLine = 3 * width
+        #qImg = QImage(cvImg.data, width, height,
+        #              bytesPerLine, QImage.Format_RGB888).rgbSwapped()
+        #pixmap01 = QPixmap.fromImage(qImg)
+        #trans = QTransform()
+        #pixmap0 = 1
+        im1 = Image.open('./pictures/sample.png')
+        canvas = Image.new('RGB', (im1.width * 6, im1.height), (255, 255, 255))
+        for i in range(6):
+            canvas.paste(im1, (im1.width * i, 0))
+        #im = im.rotate(45, expand=True)
+        canvas = canvas.rotate(30.5, expand=True)
+        canvas.show()
+        #im1.show()
+        #qim = ImageQt(canvas)
+        #
+        #pixmap01 = QPixmap.fromImage(qim)
+        #lbl.setPixmap(pixmap01)
+        self.imageArea.addWidget(lbl)
 
 
 if __name__ == '__main__':
